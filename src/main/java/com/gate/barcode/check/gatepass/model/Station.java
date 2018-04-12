09b22d1b160aa17dd45d10f33813cfb872b6b742
Munal <dhakal.munal@gmail.com>
2018-04-12 11:51:05 +0545

    Some columns added on entities

--- src/main/java/com/gate/barcode/check/gatepass/model/Station.java
+++ src/main/java/com/gate/barcode/check/gatepass/model/Station.java
@@ -16,23 +16,37 @@ import javax.persistence.Table;
  * @since , 11 Apr 2018
  */
 @Entity
-@Table(name="station")
+@Table(name = "station")
 public class Station implements Serializable {
 	@Id
 	@Column(name = "id")
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String stationName;
+	private Long stationMaster;
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public String getStationName() {
 		return stationName;
 	}
+
 	public void setStationName(String stationName) {
 		this.stationName = stationName;
 	}
+
+	public Long getStationMaster() {
+		return stationMaster;
+	}
+
+	public void setStationMaster(Long stationMaster) {
+		this.stationMaster = stationMaster;
+	}
+	
 }
